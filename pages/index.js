55bea51235a51109f3d15aae93b225d00469d640
Dan Miles <dan@slide.so>
2022-10-02 18:45:17 -0700

    init redirect

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <head>
+        <title>This isn't real</title>
+        <meta httpEquiv='refresh' content='0; URL=https://dmiles.dev'/>
+    </head>
+  )
+}
